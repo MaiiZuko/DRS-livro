b8899e48845982bb6712ae3202356f39b3bc505c
MaiiZuko <maiaramzucco@gmail.com>
2023-12-28 11:26:29 -0300

    update

--- src/main/java/com/book/capitulo2/Extrato.java
+++ src/main/java/com/book/capitulo2/Extrato.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.time.Month;
 
 public class Extrato {
-    private static final String DADOS_PADRAO = "C:\\DRS-livro\\extrato.csv";
+    private static final String DADOS_PADRAO = "C:\\Users\\maiar\\DRS-livro\\extrato.csv";
 
 
     public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
